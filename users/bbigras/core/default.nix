71095ba6c03bfd3b260852167189fd9a5b787757
Bruno Bigras <bigras.bruno@gmail.com>
2020-09-15 03:22:22 -0400

    fix build

--- users/bbigras/core/default.nix
+++ users/bbigras/core/default.nix
@@ -4,7 +4,7 @@ let
 
   neuron = (import (import ../../../nix).neuron { });
   crate2nix = (import (import ../../../nix).crate2nix { });
-  manix = (import (import ../../../nix).manix { });
+  manix = (import (import ../../../nix).manix { inherit pkgs; });
 
   my_cdda = pkgs.cataclysm-dda.withMods (mods: with mods; [
     tileset.UndeadPeople
