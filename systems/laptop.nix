6c95733bf54067739a207b2edd27316c07a62e18
Bruno Bigras <bigras.bruno@gmail.com>
2020-09-15 11:25:51 -0400

    laptop: tlp: extraConfig -> settings

--- systems/laptop.nix
+++ systems/laptop.nix
@@ -100,30 +100,30 @@ in
     # fwupd.enable = true;
     tlp = {
       enable = true;
-      extraConfig = ''
-        CPU_ENERGY_PERF_POLICY_ON_AC=performance
-        CPU_ENERGY_PERF_POLICY_ON_BAT=power
-        CPU_MAX_PERF_ON_AC=100
-        CPU_MAX_PERF_ON_BAT=50
-        CPU_SCALING_GOVERNOR_ON_AC=performance
-        CPU_SCALING_GOVERNOR_ON_BAT=powersave
+      settings = {
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MAX_PERF_ON_BAT = 50;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE="bluetooth wifi"
-        DEVICES_TO_ENABLE_ON_AC="bluetooth wifi"
+        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth wifi";
+        DEVICES_TO_ENABLE_ON_AC = "bluetooth wifi";
 
-        DISK_DEVICES="sda"
-        DISK_IOSCHED="mq-deadline bfq"
+        DISK_DEVICES = "sda";
+        DISK_IOSCHED = "mq-deadline bfq";
 
-        MAX_LOST_WORK_SECS_ON_AC=15
-        MAX_LOST_WORK_SECS_ON_BAT=15
+        MAX_LOST_WORK_SECS_ON_AC = 15;
+        MAX_LOST_WORK_SECS_ON_BAT = 15;
 
-        RUNTIME_PM_ON_AC=auto
-        RUNTIME_PM_ON_BAT=auto
+        RUNTIME_PM_ON_AC = "auto";
+        RUNTIME_PM_ON_BAT = "auto";
 
-        SOUND_POWER_SAVE_ON_AC=1
-        SOUND_POWER_SAVE_ON_BAT=1
-        SOUND_POWER_SAVE_CONTROLLER=Y
-      '';
+        SOUND_POWER_SAVE_ON_AC = 1;
+        SOUND_POWER_SAVE_ON_BAT = 1;
+        SOUND_POWER_SAVE_CONTROLLER = "Y";
+      };
     };
   };
 
