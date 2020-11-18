cf73b49216b492c17ed4f74ed42186b720f3003a
Bruno Bigras <bigras.bruno@gmail.com>
2020-11-18 15:22:14 -0500

    emacs: use fira-code-mode from melpa

--- users/bbigras/dev/emacs.nix
+++ users/bbigras/dev/emacs.nix
@@ -173,72 +173,12 @@ in
                   (goto-char (point-max))
                   (insert "\n"))))))
 
-      (defun fira-code-mode--make-alist (list)
-        "Generate prettify-symbols alist from LIST."
-        (let ((idx -1))
-          (mapcar
-           (lambda (s)
-             (setq idx (1+ idx))
-             (let* ((code (+ #Xe100 idx))
-                (width (string-width s))
-                (prefix ())
-                (suffix '(?\s (Br . Br)))
-                (n 1))
-           (while (< n width)
-             (setq prefix (append prefix '(?\s (Br . Bl))))
-             (setq n (1+ n)))
-           (cons s (append prefix suffix (list (decode-char 'ucs code))))))
-           list)))
-
-      (defconst fira-code-mode--ligatures
-        '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\"
-          "{-" "[]" "::" ":::" ":=" "!!" "!=" "!==" "-}"
-          "--" "---" "-->" "->" "->>" "-<" "-<<" "-~"
-          "#{" "#[" "##" "###" "####" "#(" "#?" "#_" "#_("
-          ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*"
-          "/**" "/=" "/==" "/>" "//" "///" "&&" "||" "||="
-          "|=" "|>" "^=" "$>" "++" "+++" "+>" "=:=" "=="
-          "===" "==>" "=>" "=>>" "<=" "=<<" "=/=" ">-" ">="
-          ">=>" ">>" ">>-" ">>=" ">>>" "<*" "<*>" "<|" "<|>"
-          "<$" "<$>" "<!--" "<-" "<--" "<->" "<+" "<+>" "<="
-          "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<" "<~"
-          "<~~" "</" "</>" "~@" "~-" "~=" "~>" "~~" "~~>" "%%"
-          "x" ":" "+" "+" "*"))
-
-      (defvar fira-code-mode--old-prettify-alist)
-
-      (defun fira-code-mode--enable ()
-        "Enable Fira Code ligatures in current buffer."
-        (setq-local fira-code-mode--old-prettify-alist prettify-symbols-alist)
-        (setq-local prettify-symbols-alist (append (fira-code-mode--make-alist fira-code-mode--ligatures) fira-code-mode--old-prettify-alist))
-        (prettify-symbols-mode t))
-
-      (defun fira-code-mode--disable ()
-        "Disable Fira Code ligatures in current buffer."
-        (setq-local prettify-symbols-alist fira-code-mode--old-prettify-alist)
-        (prettify-symbols-mode -1))
-
-      (define-minor-mode fira-code-mode
-        "Fira Code ligatures minor mode"
-        :lighter " Fira Code"
-        (setq-local prettify-symbols-unprettify-at-point 'right-edge)
-        (if fira-code-mode
-            (fira-code-mode--enable)
-          (fira-code-mode--disable)))
-
-      (defun fira-code-mode--setup ()
-        "Setup Fira Code Symbols"
-        (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol"))
-
-      (provide 'fira-code-mode)
-
       (defun rah-prog-mode-setup ()
         ;; Use a bit wider fill column width in programming modes
         ;; since we often work with indentation to start with.
         (setq fill-column 80))
 
       (add-hook 'prog-mode-hook #'rah-prog-mode-setup)
-      (add-hook 'prog-mode-hook #'fira-code-mode)
 
       (defun rah-sort-lines-ignore-case ()
         (interactive)
@@ -499,6 +439,11 @@ in
         defer = true;
         # Avoid spamming reload requests of TAGS files.
         config = "(setq tags-revert-without-query t)";
+      };
+
+      fira-code-mode = {
+        enable = true;
+        hook = [ "prog-mode" ];
       };
 
       ggtags = {
