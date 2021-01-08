d71f032875667d3f62eb4e24601fcd7960adf3b7
Bruno Bigras <bigras.bruno@gmail.com>
2021-01-08 14:32:32 -0500

    emacs: + embark

--- users/bbigras/dev/emacs.nix
+++ users/bbigras/dev/emacs.nix
@@ -808,6 +808,34 @@
         '';
       };
 
+      embark = {
+        enable = true;
+        bind = {
+          "C-S-a" = "embark-act";
+        };
+        config = ''
+          ;; For Selectrum users:
+          (defun current-candidate+category ()
+            (when selectrum-active-p
+              (cons (selectrum--get-meta 'category)
+                    (selectrum-get-current-candidate))))
+
+          (add-hook 'embark-target-finders #'current-candidate+category)
+
+          (defun current-candidates+category ()
+            (when selectrum-active-p
+              (cons (selectrum--get-meta 'category)
+                    (selectrum-get-current-candidates
+                     ;; Pass relative file names for dired.
+                     minibuffer-completing-file-name))))
+
+          (add-hook 'embark-candidate-collectors #'current-candidates+category)
+
+          ;; No unnecessary computation delay after injection.
+          (add-hook 'embark-setup-hook 'selectrum-set-selected-candidate)
+        '';
+      };
+
       nyan-mode = {
         enable = true;
         command = [ "nyan-mode" ];
