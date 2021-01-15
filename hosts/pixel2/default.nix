0fe1b8ec1d5a905438bd0db23ee81cb0f50195e0
Bruno Bigras <bigras.bruno@gmail.com>
2021-01-15 10:08:17 -0500

    pixel 2: git config

--- hosts/pixel2/default.nix
+++ hosts/pixel2/default.nix
@@ -61,7 +61,23 @@
         };
         bat.enable = true;
         command-not-found.enable = true;
-        git.enable = true;
+        git = {
+          enable = true;
+          delta.enable = true;
+          userName = "Bruno Bigras";
+          userEmail = "bigras.bruno@gmail.com";
+          aliases = {
+            st = "status";
+            co = "checkout";
+            ci = "commit";
+            br = "branch";
+            lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+            recent = "for-each-ref --sort=-committerdate --format='%(committerdate:short): %(refname:short)' refs/heads/";
+          };
+          extraConfig = {
+            pull.ff = "only";
+          };
+        };
         jq.enable = true;
         mcfly.enable = true;
         ssh.enable = true;
