98e1f94190dd06d0ca679214024d23f1dc515e63
Bernardo Meurer <bernardo@meurer.org>
2021-02-15 21:36:34 -0800

    changed: enable gnome keyring on sway/trusted
    (cherry picked from commit f779ba08d0472a104032dd76da8c7cce140d970b)

--- sway/trusted.nix
+++ sway/trusted.nix
@@ -1,3 +1,13 @@
 { pkgs, ... }: {
-  services.dbus.packages = with pkgs; [ gcr ];
+  programs = {
+    seahorse.enable = true;
+    ssh.startAgent = true;
+  };
+
+  security.pam.services.login.enableGnomeKeyring = true;
+
+  services = {
+    dbus.packages = with pkgs; [ gcr ];
+    gnome3.gnome-keyring.enable = true;
+  };
 }
