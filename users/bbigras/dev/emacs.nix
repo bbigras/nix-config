12b4170befd1efee1df2e1aa658e156cd42928a7
Bruno Bigras <bigras.bruno@gmail.com>
2021-06-12 02:56:11 -0400

    emacs: update bindings

--- users/bbigras/dev/emacs.nix
+++ users/bbigras/dev/emacs.nix
@@ -761,27 +761,48 @@ in
       consult = {
         enable = true;
         bind = {
-          "C-x M-:" = "consult-complex-command";
           "C-c h" = "consult-history";
           "C-c m" = "consult-mode-command";
-          "C-x b" = "consult-buffer";
-          "C-x 4 b" = "consult-buffer-other-window";
-          "C-x 5 b" = "consult-buffer-other-frame";
-          "C-x r x" = "consult-register";
-          "C-x r b" = "consult-bookmark";
-          "M-g g" = "consult-goto-line";
-          "M-g M-g" = "consult-goto-line";
-          "M-g o" = "consult-outline"; # "M-s o" is a good alternative.
-          "M-g l" = "consult-line"; # "M-s l" is a good alternative.
-          "M-g m" = "consult-mark"; # I recommend to bind Consult navigation
-          "M-g k" = "consult-global-mark"; # commands under the "M-g" prefix.
-          "M-g r" = "consult-git-grep"; # or consult-grep, consult-ripgrep
-          "M-g f" = "consult-find"; # or consult-locate, my-fdfind
-          "M-g i" = "consult-project-imenu"; # or consult-imenu
-          "M-g e" = "consult-error";
+          "C-c b" = "consult-bookmark";
+          "C-c k" = "consult-kmacro";
+          # C-x bindings (ctl-x-map);
+          "C-x M-:" = "consult-complex-command"; # orig. repeat-complex-command
+          "C-x b" = "consult-buffer"; # orig. switch-to-buffer
+          "C-x 4 b" = "consult-buffer-other-window"; # orig. switch-to-buffer-other-window
+          "C-x 5 b" = "consult-buffer-other-frame"; # orig. switch-to-buffer-other-frame
+          # Custom M-# bindings for fast register access
+          "M-#" = "consult-register-load";
+          "M-'" = "consult-register-store"; # orig. abbrev-prefix-mark (unrelated)
+          "C-M-#" = "consult-register";
+          # Other custom bindings
+          "M-y" = "consult-yank-pop"; # orig. yank-pop
+          "<help> a" = "consult-apropos"; # orig. apropos-command
+          # M-g bindings (goto-map)
+          "M-g e" = "consult-compile-error";
+          "M-g f" = "consult-flymake"; # Alternative: consult-flycheck
+          "M-g g" = "consult-goto-line"; # orig. goto-line
+          "M-g M-g" = "consult-goto-line"; # orig. goto-line
+          "M-g o" = "consult-outline";
+          "M-g m" = "consult-mark";
+          "M-g k" = "consult-global-mark";
+          "M-g i" = "consult-imenu";
+          "M-g I" = "consult-project-imenu";
+          # M-s bindings (search-map)
+          "M-s f" = "consult-find";
+          "M-s L" = "consult-locate";
+          # "M-s g" = "consult-grep";
+          "M-s G" = "consult-git-grep";
+          "M-s r" = "consult-ripgrep";
+          "M-s l" = "consult-line";
           "M-s m" = "consult-multi-occur";
-          "M-y" = "consult-yank-pop";
-          "<help> a" = "consult-apropos";
+          "M-s k" = "consult-keep-lines";
+          "M-s u" = "consult-focus-lines";
+          # Isearch integration
+          "M-s e" = "consult-isearch";
+          # :map isearch-mode-map
+          #"M-e" = "consult-isearch";                 # orig. isearch-edit-string
+          #"M-s e" = "consult-isearch";               # orig. isearch-edit-string
+          #"M-s l" = "consult-line";                 # required by consult-line to detect isearch
         };
         init = ''
           (fset 'multi-occur #'consult-multi-occur)
@@ -834,6 +855,9 @@ in
 
       marginalia = {
         enable = true;
+        bind = {
+          "M-A" = "marginalia-cycle";
+        };
         config = ''
           (marginalia-mode)
           (setq marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light))
