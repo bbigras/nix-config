3d1b43c43660cfff2fe0dfcebc55c265a1f4ff18
Bruno Bigras <bigras.bruno@gmail.com>
2021-09-23 13:31:31 -0400

    fix build

--- dev/virt-manager.nix
+++ dev/virt-manager.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ virt-manager spice-gtk ];
   security.polkit.enable = true;
-  security.wrappers.spice-client-glib-usb-acl-helper.source = "${pkgs.spice-gtk}/bin/spice-client-glib-usb-acl-helper";
+  #security.wrappers.spice-client-glib-usb-acl-helper.source = "${pkgs.spice-gtk}/bin/spice-client-glib-usb-acl-helper";
 }
