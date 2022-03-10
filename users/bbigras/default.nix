7b060345e7b6fe0956567997c1a3c5c803528b1d
Bruno Bigras <bigras.bruno@gmail.com>
2022-03-10 14:19:43 -0500

    bbigras: add pixel6 ssh pubkey

--- users/bbigras/default.nix
+++ users/bbigras/default.nix
@@ -21,6 +21,7 @@ with lib;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2Eo0xWZ1VPs5iHlDd3j+O+3I1qx4VqDaXpkL6phB6Z bbigras@desktop"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6LFIPxHhM18nw6Sp8xPVG2GGPNcTSrNwTQAoNIyA0r bbigras@laptop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAoSe4gCERcN4Fucwd7dFWUm9LLO/T0wBC+NzXcgrhwt bbigras@pixel6"
     ];
   };
 
