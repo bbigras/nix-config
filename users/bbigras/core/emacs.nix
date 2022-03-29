ce12be6af806e2217ffd17487fdac671e0ac5e40
Bruno Bigras <bigras.bruno@gmail.com>
2022-03-29 14:37:31 -0400

    emacs: set magit-repolist-columns

--- users/bbigras/core/emacs.nix
+++ users/bbigras/core/emacs.nix
@@ -997,6 +997,17 @@ in
           config = ''
             (add-to-list 'git-commit-style-convention-checks
                          'overlong-summary-line)
+            (setq magit-repolist-columns
+                  '(("Name"    25 magit-repolist-column-ident ())
+                    ("Version" 25 magit-repolist-column-version ())
+                    ("D"        1 magit-repolist-column-dirty ())
+                    ("⇣"      3 magit-repolist-column-unpulled-from-upstream
+                     ((:right-align t)
+                      (:help-echo "Upstream changes not in branch")))
+                    ("⇡"        3 magit-repolist-column-unpushed-to-upstream
+                     ((:right-align t)
+                      (:help-echo "Local changes not in upstream")))
+                    ("Path"    99 magit-repolist-column-path ())))
           '';
         };
 
