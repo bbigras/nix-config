d4c006228363787adc9abee59a52d37604cb8c1b
Bruno Bigras <bigras.bruno@gmail.com>
2022-04-21 03:38:05 -0400

    sync with upstream

--- users/bbigras/core/git.nix
+++ users/bbigras/core/git.nix
@@ -1,23 +1,30 @@
 { pkgs, ... }: {
   programs.git = {
     enable = true;
+    delta = {
+      enable = true;
+      options = {
+        navigate = true;
+        syntax-theme = "Nord";
+      };
+    };
     package = pkgs.gitFull;
     lfs.enable = false;
     userEmail = "bigras.bruno@gmail.com";
     userName = "Bruno Bigras";
-
-    ignores = [
-      "*~"
-      "*.swp"
-      ".direnv/"
-      # exclude nix-build result
-      "result"
-      "result-*"
-    ];
-
-    delta = {
-      enable = true;
-      options.syntax-theme = "Nord";
+    extraConfig = {
+      diff.colorMoved = "default";
+      difftool.prompt = true;
+      github.user = "bbigras";
+      init.defaultBranch = "main";
+      merge.conflictstyle = "zdiff3";
+      mergetool.prompt = true;
+      pull.ff = "only";
+      credential.helper = "${pkgs.gitAndTools.gitFull}/bin/git-credential-libsecret";
+      diff.tool = "diffsitter";
+      # difftool.prompt = false;
+      difftool.difftastic.cmd = "/home/bbigras/.cargo/bin/difftastic \"$LOCAL\" \"$REMOTE\"";
+      difftool.diffsitter.cmd = "/home/bbigras/.cargo/bin/diffsitter \"$LOCAL\" \"$REMOTE\"";
     };
 
     aliases = {
@@ -29,20 +36,12 @@
       recent = "for-each-ref --sort=-committerdate --format='%(committerdate:short): %(refname:short)' refs/heads/";
     };
 
-    extraConfig = {
-      # core.pager = "${pkgs.delta}/bin/delta --dark";
-      difftool.prompt = true;
-      github.user = "bbigras";
-      mergetool.prompt = true;
-      init.defaultBranch = "master";
-      #
-      pull.ff = "only";
-      merge.conflictstyle = "diff3";
-      credential.helper = "${pkgs.gitAndTools.gitFull}/bin/git-credential-libsecret";
-      diff.tool = "diffsitter";
-      # difftool.prompt = false;
-      difftool.difftastic.cmd = "/home/bbigras/.cargo/bin/difftastic \"$LOCAL\" \"$REMOTE\"";
-      difftool.diffsitter.cmd = "/home/bbigras/.cargo/bin/diffsitter \"$LOCAL\" \"$REMOTE\"";
-    };
+    ignores = [
+      "*~"
+      "*.swp"
+      # exclude nix-build result
+      "result"
+      "result-*"
+    ];
   };
 }
