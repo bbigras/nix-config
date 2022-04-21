75b34fea728bd58f9407fb68c7aa2fc599041e53
Bruno Bigras <bigras.bruno@gmail.com>
2022-04-21 03:41:33 -0400

    sync with upstream

--- graphical/default.nix
+++ graphical/default.nix
@@ -5,42 +5,15 @@
     # ./greetd.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    adwaita-qt
-    gnome.adwaita-icon-theme
-    hicolor-icon-theme
-    qgnomeplatform
-    qt5.qtwayland
-    spawn
-  ];
-
-  qt5 = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
-  };
-
-  environment.etc."sway/config.d/gtk.conf".text = ''
-    exec systemctl --user import-environment DISPLAY WAYLAND_DISPLAY SWAYSOCK
-    exec hash dbus-update-activation-environment 2>/dev/null && \
-      dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
-  '';
-
   programs.dconf.enable = true;
 
-  services = {
-    dbus.packages = with pkgs; [ dconf ];
-    # redshift = {
-    #   enable = true;
-    #   package = pkgs.redshift-wlr;
-    #   extraOptions = [ "-v" ];
-    # };
-  };
+  services.dbus.packages = with pkgs; [ dconf ];
+  services.gnome.at-spi2-core.enable = true;
 
-  xdg = {
-    autostart.enable = true;
-    icons.enable = true;
-    menus.enable = true;
-    mime.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    gtkUsePortal = true;
+    wlr.enable = true;
   };
 }
