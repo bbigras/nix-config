83bc0534e69b986e3d7b49f11ab5853de1aedf0f
Bruno Bigras <bigras.bruno@gmail.com>
2022-04-21 03:10:57 -0400

    dev: remove dictd and some udev rule

--- dev/default.nix
+++ dev/default.nix
@@ -9,10 +9,4 @@
   programs = {
     wireshark.enable = true;
   };
-
-  services.dictd.enable = true;
-
-  services.udev.extraRules = ''
-    ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", MODE="0660", GROUP="dialout", TAG+="uaccess"
-  '';
 }
