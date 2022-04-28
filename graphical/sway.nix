d8424e4271d888a43b26f01bf48d3c4bb8dc8749
Bruno Bigras <bigras.bruno@gmail.com>
2022-04-28 12:12:10 -0400

    sway: addback xdg.portal

--- graphical/sway.nix
+++ graphical/sway.nix
@@ -1,3 +1,12 @@
+{ pkgs, ... }:
+
 {
   programs.sway.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    gtkUsePortal = true;
+    wlr.enable = true;
+  };
 }
