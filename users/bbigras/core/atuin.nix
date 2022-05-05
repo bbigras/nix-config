de09b97ebe83fa233644fc4f0fbdc4eb560a84ea
Bruno Bigras <bigras.bruno@gmail.com>
2022-05-05 00:58:29 -0400

    atuin: don't set ATUIN_NOBIND

--- users/bbigras/core/atuin.nix
+++ users/bbigras/core/atuin.nix
@@ -3,21 +3,4 @@
     enable = true;
     settings.auto_sync = true;
   };
-  programs.bash = {
-    bashrcExtra = ''
-      export ATUIN_NOBIND="true"
-    '';
-    initExtra = ''
-      bind -x '"\C-r": __atuin_history'
-    '';
-  };
-
-  programs.zsh = {
-    envExtra = ''
-      export ATUIN_NOBIND="true"
-    '';
-    initExtra = ''
-      bindkey '^r' _atuin_search_widget
-    '';
-  };
 }
