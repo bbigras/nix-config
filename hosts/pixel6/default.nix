8b33e7d5f915b8c18826e3b7a8168272802c009d
Bruno Bigras <bigras.bruno@gmail.com>
2022-05-05 13:25:30 -0400

    pixel6: auto install font

--- hosts/pixel6/default.nix
+++ hosts/pixel6/default.nix
@@ -56,10 +56,22 @@
   home-manager.useGlobalPkgs = true;
 
   home-manager.config =
-    { pkgs, ... }:
+    { pkgs, config, lib, ... }:
     {
       # Read the changelog before changing this value
       home.stateVersion = "20.09";
+
+      home.activation = {
+        copyFont =
+          let
+            font_src = "${pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }}/share/fonts/truetype/NerdFonts/Fira Code Regular Nerd Font Complete Mono.ttf";
+            # font_src = "${pkgs.meslo-lgs-nf}}/share/fonts/truetype/MesloLGS NF Regular.ttf";
+            font_dst = "${config.home.homeDirectory}/.termux/font.ttf";
+          in
+          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            ( test ! -e "${font_dst}" || test $(sha1sum "${font_src}"|cut -d' ' -f1 ) != $(sha1sum "${font_dst}" |cut -d' ' -f1)) && $DRY_RUN_CMD install $VERBOSE_ARG -D "${font_src}" "${font_dst}"
+          '';
+      };
 
       imports = [
         ../../users/bbigras/core/atuin.nix
