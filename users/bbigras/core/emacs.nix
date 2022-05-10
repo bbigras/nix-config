65186a8f41ac72a049b6a6b064606ff022e6ab59
Bruno Bigras <bigras.bruno@gmail.com>
2022-05-09 22:01:33 -0400

    emacs: defun ora-ediff-files

--- users/bbigras/core/emacs.nix
+++ users/bbigras/core/emacs.nix
@@ -1817,6 +1817,28 @@ in
             (setq delete-by-moving-to-trash t)
             (setq dired-dwim-target t)
             (setq dired-listing-switches "-agho --group-directories-first")
+
+            (defun ora-ediff-files ()
+              (interactive)
+              (let ((files (dired-get-marked-files))
+                    (wnd (current-window-configuration)))
+                (if (<= (length files) 2)
+                    (let ((file1 (car files))
+                          (file2 (if (cdr files)
+                                     (cadr files)
+                                   (read-file-name
+                                    "file: "
+                                    (dired-dwim-target-directory)))))
+                      (if (file-newer-than-file-p file1 file2)
+                          (ediff-files file2 file1)
+                        (ediff-files file1 file2))
+                      (add-hook 'ediff-after-quit-hook-internal
+                                (lambda ()
+                                  (setq ediff-after-quit-hook-internal nil)
+                                  (set-window-configuration wnd))))
+                  (error "no more than 2 files should be marked"))))
+
+            (define-key dired-mode-map "e" 'ora-ediff-files)
           '';
         };
 
