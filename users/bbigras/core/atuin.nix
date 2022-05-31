c7626251a56cf58317111be1e4c3608d022dd7a4
Bruno Bigras <bigras.bruno@gmail.com>
2022-05-31 11:12:54 -0400

    atuin: ATUIN_NOBIND

--- users/bbigras/core/atuin.nix
+++ users/bbigras/core/atuin.nix
@@ -2,4 +2,22 @@
   programs.atuin = {
     enable = true;
   };
+
+  programs.bash = {
+    bashrcExtra = ''
+      export ATUIN_NOBIND="true"
+    '';
+    initExtra = ''
+      bind -x '"\C-r": __atuin_history'
+    '';
+  };
+
+  programs.zsh = {
+    envExtra = ''
+      export ATUIN_NOBIND="true"
+    '';
+    initExtra = ''
+      bindkey '^r' _atuin_search_widget
+    '';
+  };
 }
