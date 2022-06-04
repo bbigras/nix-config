9888dd3f526aa5aa22ec11530b4f0cde9ecdefd8
Bruno Bigras <bigras.bruno@gmail.com>
2022-06-04 14:43:56 -0400

    virt-manager: fix build

--- dev/virt-manager.nix
+++ dev/virt-manager.nix
@@ -8,7 +8,7 @@
         package = pkgs.qemu_kvm;
         ovmf = {
           enable = true;
-          package = pkgs.OVMFFull;
+          packages = [ pkgs.OVMFFull ];
         };
         swtpm.enable = true;
       };
