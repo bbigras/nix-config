aa453bf20d6cd13c7a4e07848318cca713e88220
Bruno Bigras <bigras.bruno@gmail.com>
2022-08-25 22:43:07 -0400

    yggdrasil: fix secret

--- core/yggdrasil.nix
+++ core/yggdrasil.nix
@@ -1,6 +1,11 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 
 {
+  sops.secrets = {
+    yggdrasil-conf.owner = config.users.users.yggdrasil.name;
+    # yggdrasil-conf.group = config.users.groups.yggdrasil.name;
+  };
+
   networking.firewall.allowedTCPPorts = [ 9002 ];
   networking.firewall.allowedUDPPorts = [ 9001 ];
 
@@ -12,4 +17,19 @@
     enable = true;
     configFile = config.sops.secrets.yggdrasil-conf.path;
   };
+
+  systemd.services.yggdrasil = {
+    serviceConfig.DynamicUser = lib.mkForce false;
+    serviceConfig.SupplementaryGroups = [ config.users.groups.keys.name ];
+  };
+
+  systemd.services.yggdrasil.serviceConfig.User = config.users.users.yggdrasil.name;
+  systemd.services.yggdrasil.serviceConfig.Group = "yggdrasil";
+
+  users.users.yggdrasil = {
+    isSystemUser = true;
+  };
+
+  users.users.yggdrasil.group = "yggdrasil";
+  users.groups.yggdrasil = { };
 }
