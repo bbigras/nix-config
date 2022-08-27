936a5385ab7611ad61a7b2ecef1044233666b135
Bruno Bigras <bigras.bruno@gmail.com>
2022-08-27 10:56:46 -0400

    add: fira-code

--- dev/default.nix
+++ dev/default.nix
@@ -1,7 +1,15 @@
+{ pkgs, ... }:
+
 {
   imports = [ ./documentation.nix ];
 
   environment = {
     enableDebugInfo = true;
   };
+
+  fonts.fonts = with pkgs; [
+    fira-code
+    fira-code-symbols
+    # meslo-lgs-nf
+  ];
 }
