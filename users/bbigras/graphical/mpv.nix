872d786a09e87f4ff1c69afc54a9147eb3424701
Bruno Bigras <bigras.bruno@gmail.com>
2022-11-19 13:25:40 -0500

    mpv: add scripts

--- users/bbigras/graphical/mpv.nix
+++ users/bbigras/graphical/mpv.nix
@@ -1,11 +1,30 @@
+{ pkgs, ... }:
+
 {
   programs.mpv = {
     enable = true;
+    package = pkgs.mpv.override {
+      scripts = with pkgs.mpvScripts; [
+        mpris
+        # sponsorblock
+        thumbnail
+        youtube-quality
+      ];
+    };
+    bindings = {
+      # WHEEL_UP = "seek 10";
+      # WHEEL_DOWN = "seek -10";
+      "Alt+0" = "set window-scale 0.5";
+    };
     config = {
       profile = "gpu-hq";
       #gpu-context = "wayland";
       vo = "gpu";
       hwdec = "auto";
+      force-window = true;
+      # ytdl-format = "bestvideo+bestaudio";
+      # cache-default = 4000000;
+      osc = "no"; # for mpvScripts.thumbnail
     };
   };
 
