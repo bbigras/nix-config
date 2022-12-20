6cb6048aa02113a1da5707cfa4c493f308ff749d
Bruno Bigras <bigras.bruno@gmail.com>
2022-12-20 13:15:28 -0500

    enable pixel6 again

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -1,6 +1,7 @@
 { self
 , deploy-rs
 , nixpkgs
+, nix-on-droid
 , ...
 }:
 let
@@ -17,6 +18,15 @@ let
       hostname = address;
       profiles.system.path = activate.nixos nixosCfg;
     };
+
+  pkgs_arch64 = import nixpkgs {
+    system = "aarch64-linux";
+  };
+
+  pixel6 = (nix-on-droid.lib.nixOnDroidConfiguration {
+    pkgs = pkgs_arch64;
+    modules = [ ../hosts/pixel6 ];
+  }).activationPackage;
 in
 {
   # XXX: auto-rollback is too noisy since any service failing will cause it to
@@ -24,5 +34,17 @@ in
   autoRollback = false;
   magicRollback = true;
   user = "root";
-  nodes = lib.mapAttrs genNode self.nixosConfigurations;
+  nodes = lib.mapAttrs genNode self.nixosConfigurations // {
+    pixel6 = {
+      hostname = "pixel6";
+
+      # to prevent using sudo
+      sshUser = "nix-on-droid";
+      user = "nix-on-droid";
+
+      profiles.system.path = deploy-rs.lib.aarch64-linux.activate.custom
+        pixel6
+        (pixel6 + "/activate");
+    };
+  };
 }
