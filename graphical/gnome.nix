894bfb974eb55419e9756d69cc4219792fdbe097
Bruno Bigras <bigras.bruno@gmail.com>
2022-12-19 23:56:30 -0500

    gnome: remove evolution-data-server

--- graphical/gnome.nix
+++ graphical/gnome.nix
@@ -8,7 +8,6 @@
   services = {
     accounts-daemon.enable = true;
     gnome = {
-      evolution-data-server.enable = true;
       gnome-keyring.enable = true;
       gnome-online-accounts.enable = true;
       gnome-online-miners.enable = true;
