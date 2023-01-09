9d56af7359c9b4680a464bb765798306e09e7c5d
Bruno Bigras <bigras.bruno@gmail.com>
2023-01-09 10:42:43 -0500

    Revert "emacs: remove org-roam"
    This reverts commit 7cf31ba33155ccb2e94dc48a1b19eddb1f08505f.

--- users/bbigras/core/emacs.nix
+++ users/bbigras/core/emacs.nix
@@ -465,6 +465,22 @@ in
         #       '';
         #     };
 
+        org-roam = {
+          enable = true;
+          after = [ "org" "org-element" "emacsql" ];
+          hook = [ "(after-init . org-roam-mode)" ];
+          config = ''
+            (setq org-roam-directory "~/dev/org-mode/notes")
+          '';
+          bind = {
+            "C-c n l" = "org-roam";
+            "C-c n t" = "org-roam-dailies-find-today";
+            "C-c n f" = "org-roam-find-file";
+            "C-c n i" = "org-roam-insert";
+            "C-c n g" = "org-roam-graph";
+          };
+        };
+
         rainbow-mode = {
           enable = true;
           defer = 1;
