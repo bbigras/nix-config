441dc4579171394760c0970677eb95e09a113a0d
Bruno Bigras <bigras.bruno@gmail.com>
2023-01-26 09:24:43 -0500

    tweak fzf

--- hosts/pixel6/default.nix
+++ hosts/pixel6/default.nix
@@ -101,7 +101,30 @@
           enable = true;
           enableAliases = true;
         };
-        fzf.enable = true;
+        fzf = {
+          enable = true;
+          tmux.enableShellIntegration = true;
+          tmux.shellIntegrationOptions = [ "-d 40%" ];
+          defaultCommand = "fd --type f";
+          defaultOptions = [
+            "--height 40%"
+            "--border"
+          ];
+          changeDirWidgetCommand = "fd --type d"; # alt+c
+          changeDirWidgetOptions = [
+            "--preview 'tree -C {} | head -200'"
+          ];
+          fileWidgetCommand = "fd --type f";
+          fileWidgetOptions = [
+            "--preview 'head {}'"
+          ];
+          colors = {
+            bg = "#1e1e1e";
+            "bg+" = "#1e1e1e";
+            fg = "#d4d4d4";
+            "fg+" = "#d4d4d4";
+          };
+        };
         jq.enable = true;
         htop.enable = true;
         nushell.enable = false;
