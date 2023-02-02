5dfc776d24ba913b2efc1b1a62bde30046ef8df7
Bruno Bigras <bigras.bruno@gmail.com>
2023-02-01 23:46:12 -0500

    emacs: fix embark

--- users/bbigras/core/emacs.nix
+++ users/bbigras/core/emacs.nix
@@ -933,24 +933,6 @@ in
 
             (add-to-list 'embark-keymap-alist '(this-buffer-file . this-buffer-file-map))
 
-            (embark-define-keymap this-buffer-file-map
-                  "Commands to act on current file or buffer."
-                  ("l" load-file)
-                  ("b" byte-compile-file)
-                  ("S" sudo-find-file)
-                  ("r" rename-file-and-buffer)
-                  ("d" diff-buffer-with-file)
-                  ("=" ediff-buffers)
-                  ("C-=" ediff-files)
-                  ("!" shell-command)
-                  ("&" async-shell-command)
-                  ("x" consult-file-externally)
-                  ("c" copy-file)
-                  ("k" kill-buffer)
-                  ("z" bury-buffer)
-                  ("|" embark-shell-command-on-buffer)
-                  ("g" revert-buffer))
-
             ;; ------------------------------------------------------------
 
 
