bffa82d3230af653fa44f999b24f25cd29ded1d2
Bruno Bigras <bigras.bruno@gmail.com>
2023-03-04 10:20:57 -0500

    remove duplicate shellAliases

--- users/bbigras/core/default.nix
+++ users/bbigras/core/default.nix
@@ -195,10 +195,7 @@
       cls = "clear";
       j = "${pkgs.just}/bin/just";
       ".j" = "${pkgs.just}/bin/just --justfile ~/.user.justfile";
-      l = "ls";
-      la = "ls --all";
       less = ''${pkgs.bat}/bin/bat --paging=always --pager "${pkgs.less}/bin/less -RF"'';
-      ls = "exa --binary --header --long --classify";
       man = "${pkgs.bat-extras.batman}/bin/batman";
     };
   };
