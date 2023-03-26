bf544de84551ad285313d7c20111dc39c65f86d5
Bruno Bigras <bigras.bruno@gmail.com>
2023-03-26 03:49:48 -0400

    temp fix for nix-on-droid

--- nix/nix-on-droid.nix
+++ nix/nix-on-droid.nix
@@ -1,14 +1,25 @@
 { nixpkgs
 , nix-on-droid
 , nur
+, emacs-overlay
 , ...
 }:
 let
   inherit (nixpkgs) lib;
   hosts = (import ./hosts.nix).nix-on-droid.all;
 
+  pkgs = import nixpkgs {
+    system = "aarch64-linux"; # FIXME
+    overlays = [
+      emacs-overlay.overlay
+    ];
+    config.allowUnfree = true;
+    config.allowAliases = true;
+  };
+
   genConfiguration = hostname: _:
     nix-on-droid.lib.nixOnDroidConfiguration {
+      inherit pkgs;
       modules = [
         (../hosts + "/${hostname}")
       ];
