a946faabca67ab0c781290664b0fe612641f9940
Bruno Bigras <bigras.bruno@gmail.com>
2023-06-12 21:31:07 -0400

    sync dev-shell with upstream

--- nix/dev-shell.nix
+++ nix/dev-shell.nix
@@ -1,45 +1,46 @@
 { self, ... }:
 
-system:
+localSystem:
 
-with self.pkgs.${system};
+with self.pkgs.${localSystem};
+{
+  default = mkShell {
+    name = "nix-config";
 
-mkShell {
-  name = "nix-config";
+    nativeBuildInputs = [
+      # Nix
+      # agenix
+      cachix
+      deploy-rs.deploy-rs
+      nil
+      nix-build-uncached
+      nix-eval-jobs
+      nixpkgs-fmt
+      statix
 
-  nativeBuildInputs = [
-    # Nix
-    # agenix
-    cachix
-    deploy-rs.deploy-rs
-    nil
-    nix-build-uncached
-    nix-eval-jobs
-    nixpkgs-fmt
-    statix
+      # Lua
+      # stylua
+      # (luajit.withPackages (p: with p; [ luacheck ]))
+      # lua-language-server
 
-    # Lua
-    stylua
-    (luajit.withPackages (p: with p; [ luacheck ]))
-    lua-language-server
+      # Shell
+      shellcheck
+      shfmt
 
-    # Shell
-    shellcheck
-    shfmt
+      # GitHub Actions
+      act
+      actionlint
+      python3Packages.pyflakes
+      shellcheck
 
-    # GitHub Actions
-    act
-    actionlint
-    python3Packages.pyflakes
-    shellcheck
+      # Misc
+      jq
+      pre-commit
+      rage
+    ];
 
-    # Misc
-    jq
-    pre-commit
-    rage
-  ];
-
-  shellHook = ''
-    ${self.checks.${system}.pre-commit-check.shellHook}
-  '';
+    shellHook = ''
+      ${self.checks.${localSystem}.pre-commit-check.shellHook}
+    '';
+  };
 }
