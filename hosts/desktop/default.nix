822c384908c13d03efdd783b764991455ef94ecc
Bruno Bigras <bigras.bruno@gmail.com>
2023-08-07 23:32:25 -0400

    desktop: don't persist ssh keys

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -211,11 +211,6 @@ rec {
     files = [
       # "/etc/machine-id"
       # "/etc/nix/id_rsa"
-
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 
