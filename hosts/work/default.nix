55d57e83f94f34083e574b748b83196dbcf2a245
Bruno Bigras <bigras.bruno@gmail.com>
2023-08-07 23:30:59 -0400

    work: add hydra

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -1,7 +1,24 @@
-{ config, lib, pkgs, nur, nixos-hardware, ... }:
+{ config, lib, pkgs, nur, nixos-hardware, attic, ... }:
 
 let
   nurNoPkgs = import nur { pkgs = null; nurpkgs = pkgs; };
+
+  my_attic = import attic;
+
+  upload_to_attic = pkgs.writeScriptBin "upload-to-attic" ''
+    #!/bin/sh
+    set -eu
+    set -f # disable globbing
+
+    # skip push if the declarative job spec
+    OUT_END=$(echo ''${OUT_PATHS: -10})
+    if [ "$OUT_END" == "-spec.json" ]; then
+    exit 0
+    fi
+
+    export HOME=/root
+    exec ${my_attic}/bin/attic push luxor $OUT_PATHS > /tmp/hydra_attic 2>&1
+  '';
 in
 {
   imports = with nixos-hardware.nixosModules;
@@ -41,9 +58,32 @@ in
   services.yggdrasil.enable = lib.mkForce false;
 
   nix = {
+    settings = {
+      trusted-users = [ "hydra-queue-runner" "hydra" "hydra-www" ];
+
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
     extraOptions = ''
+      builders-use-substitutes = true
+      post-build-hook = ${upload_to_attic}/bin/upload-to-attic
       netrc-file = ${config.sops.secrets.netrc.path};
     '';
+  };
+
+  services.hydra = {
+    enable = true;
+    hydraURL = "http://localhost:3000";
+    buildMachinesFiles = [ ];
+    useSubstitutes = true;
+    notificationSender = "fake@test.local";
   };
 
   services.smartd.enable = true;
