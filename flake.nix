90e3e2aa19c2e67cfc425a337f40ccd55efb54f9
Bruno Bigras <bigras.bruno@gmail.com>
2023-09-08 15:07:49 -0400

    use my rycee/nur-expressions fork for now

--- flake.nix
+++ flake.nix
@@ -85,7 +85,7 @@
 
     nixos-hardware.url = "nixos-hardware";
     rycee-nur-expressions = {
-      url = "gitlab:rycee/nur-expressions";
+      url = "gitlab:bbigras/nur-expressions/version";
       flake = false;
     };
 
