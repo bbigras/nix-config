b90389ec4668c17374b58735f05f4c74d7659803
Bruno Bigras <bigras.bruno@gmail.com>
2024-02-27 03:26:19 -0500

    gnome: set dconf.settings

--- users/bbigras/graphical/gnome.nix
+++ users/bbigras/graphical/gnome.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./alacritty.nix
@@ -15,6 +15,10 @@
       gnomeExtensions.appindicator
       gnomeExtensions.caffeine
       gnomeExtensions.clipboard-indicator
+      gnomeExtensions.dash-to-panel
+      gnomeExtensions.space-bar
+      gnomeExtensions.user-themes
+      gnomeExtensions.vitals
     ];
   };
 
@@ -25,6 +29,45 @@
       # gpu-context = "wayland";
       vo = "gpu";
       hwdec = "auto";
+    };
+  };
+
+  dconf.enable = lib.mkForce true;
+  dconf.settings = {
+    "org/gnome/desktop/peripherals/keyboard" = {
+      numlock-state = true;
+    };
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (lib.gvariant.mkTuple [ "xkb" "ca" ]) ];
+    };
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "Alacritty.desktop"
+        "org.gnome.Nautilus.desktop"
+        "org.remmina.Remmina.desktop"
+        "emacs.desktop"
+        "element-desktop.desktop"
+        "discord.desktop"
+      ];
+      disable-user-extensions = false;
+
+      # `gnome-extensions list` for a list
+      enabled-extensions = [
+        "Vitals@CoreCoding.com"
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "caffeine@patapon.info"
+        "dash-to-panel@jderose9.github.com"
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "space-bar@luchrioh"
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+      ];
+    };
+    "org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      workspace-names = [ "Main" ];
     };
   };
 
