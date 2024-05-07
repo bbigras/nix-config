cfd6a0da780165624056d450d661f944b36bade7
Bruno Bigras <bigras.bruno@gmail.com>
2024-05-06 23:54:39 -0400

    emacs: add dashboard

--- users/bbigras/core/emacs/default.nix
+++ users/bbigras/core/emacs/default.nix
@@ -270,6 +270,20 @@
           '';
         };
 
+        dashboard = {
+          enable = true;
+          config = ''
+            (setq dashboard-projects-backend 'project-el)
+            (setq dashboard-items '((recents   . 5)
+                                    (bookmarks . 5)
+                                    (projects  . 5)
+                                    (agenda    . 5)
+                                    (registers . 5)))
+            (setq dashboard-week-agenda t)
+            (dashboard-setup-startup-hook)
+          '';
+        };
+
         dwim-shell-command = {
           enable = true;
           extraPackages = [ pkgs.atool pkgs.zrok ];
