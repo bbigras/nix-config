9ae8472c300a8358a9ad56181c166b6d38d42887
Bruno Bigras <bigras.bruno@gmail.com>
2024-05-09 15:21:42 -0400

    fzf: remove ui tweaks

--- users/bbigras/core/default.nix
+++ users/bbigras/core/default.nix
@@ -179,20 +179,9 @@
     fzf = {
       enable = true;
       tmux.enableShellIntegration = true;
-      tmux.shellIntegrationOptions = [ "-d 40%" ];
       defaultCommand = "fd --type f";
-      defaultOptions = [
-        "--height 40%"
-        "--border"
-      ];
       changeDirWidgetCommand = "fd --type d"; # alt+c
-      changeDirWidgetOptions = [
-        "--preview 'tree -C {} | head -200'"
-      ];
       fileWidgetCommand = "fd --type f";
-      fileWidgetOptions = [
-        "--preview 'head {}'"
-      ];
       catppuccin.enable = true;
     };
     gpg.enable = true;
