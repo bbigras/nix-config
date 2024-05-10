bcd6e47154f5e724f2a2eba3c2d546d80314d31b
Bruno Bigras <bigras.bruno@gmail.com>
2024-05-10 13:16:21 -0400

    emacs: add activities

--- users/bbigras/core/emacs/default.nix
+++ users/bbigras/core/emacs/default.nix
@@ -289,6 +289,24 @@
           '';
         };
 
+        activities = {
+          enable = true;
+          bind = {
+            # "M-g d" = "dogears-go";
+            "C-x C-a C-n" = "activities-new";
+            "C-x C-a C-a" = "activities-resume";
+            "C-x C-a C-s" = "activities-suspend";
+            "C-x C-a C-k" = "activities-kill";
+            "C-x C-a RET" = "activities-switch";
+            "C-x C-a g" = "activities-revert";
+            "C-x C-a l" = "activities-list";
+          };
+          init = ''
+            ;; Prevent `edebug' default bindings from interfering.
+            (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+          '';
+        };
+
         catppuccin-theme = {
           enable = true;
           config = ''
