800a84c757c2d3e564b329bab893149ce0c78e89
Bruno Bigras <bigras.bruno@gmail.com>
2024-05-10 19:37:41 -0400

    lix: use main

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lix = {
-      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
     };
     lix-module = {
