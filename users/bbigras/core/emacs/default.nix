2ad96e2cc14cebb9cd031c5305bb664946965c01
Bruno Bigras <bigras.bruno@gmail.com>
2024-05-10 13:16:21 -0400

    emacs: remove 'emacs' block

--- users/bbigras/core/emacs/default.nix
+++ users/bbigras/core/emacs/default.nix
@@ -781,27 +781,6 @@
           '';
         };
 
-        emacs = {
-          enable = true;
-          init = ''
-            ;; Add prompt indicator to `completing-read-multiple'.
-            (defun crm-indicator (args)
-              (cons (concat "[CRM] " (car args)) (cdr args)))
-            (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-            ;; Grow and shrink minibuffer
-            ;;(setq resize-mini-windows t)
-
-            ;; Do not allow the cursor in the minibuffer prompt
-            (setq minibuffer-prompt-properties
-                  '(read-only t cursor-intangible t face minibuffer-prompt))
-            (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-            ;; Enable recursive minibuffers
-            (setq enable-recursive-minibuffers t)
-          '';
-        };
-
         ctrlf = {
           enable = true;
           config = ''
