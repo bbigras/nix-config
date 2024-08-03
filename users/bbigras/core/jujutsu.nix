a2d37ec5ae34576c466099b895bc0eac5fe70014
Bruno Bigras <bigras.bruno@gmail.com>
2024-08-03 15:14:08 -0400

    tweak Jujutsu's config

--- users/bbigras/core/jujutsu.nix
+++ users/bbigras/core/jujutsu.nix
@@ -15,7 +15,9 @@
     jujutsu = {
       enable = true;
       settings = {
+        aliases.l = [ "log" "-r" "(trunk()..@):: | (trunk()..@)-" ];
         core.fsmonitor = "watchman";
+        colors."working_copy commit_id" = { underline = true; };
         ui.allow-filesets = true;
         user = {
           name = "Bruno Bigras";
@@ -23,6 +25,31 @@
         };
         template-aliases = {
           "format_short_signature(signature)" = "signature.username()";
+        };
+        revset-aliases = {
+          "HEAD" = "@-";
+          "user()" = ''user("bigras.bruno@gmail.com")'';
+          "user(x)" = "author(x) | committer(x)";
+        };
+        templates = {
+          draft_commit_description = ''
+            concat(
+            description,
+            surround(
+                "\nJJ: This commit contains the following changes:\n", "",
+                indent("JJ:     ", diff.stat(72)),
+            ),
+            )
+          '';
+          log_node = ''
+            coalesce(
+              if(!self, "🮀"),
+              if(current_working_copy, "@"),
+              if(root, "┴"),
+              if(immutable, "●", "○"),
+            )
+          '';
+          op_log_node = ''if(current_operation, "@", "○")'';
         };
       };
     };
