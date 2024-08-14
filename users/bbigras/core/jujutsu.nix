850f17ef15ce2185f43db79b47256424d0adf296
Bruno Bigras <bigras.bruno@gmail.com>
2024-08-14 13:21:22 -0400

    jujutsu: remove some customization

--- users/bbigras/core/jujutsu.nix
+++ users/bbigras/core/jujutsu.nix
@@ -17,7 +17,6 @@
       settings = {
         aliases.l = [ "log" "-r" "(trunk()..@):: | (trunk()..@)-" ];
         core.fsmonitor = "watchman";
-        colors."working_copy commit_id" = { underline = true; };
         user = {
           name = "Bruno Bigras";
           email = "bigras.bruno@gmail.com";
@@ -29,26 +28,6 @@
           "HEAD" = "@-";
           "user()" = ''user("bigras.bruno@gmail.com")'';
           "user(x)" = "author(x) | committer(x)";
-        };
-        templates = {
-          draft_commit_description = ''
-            concat(
-            description,
-            surround(
-                "\nJJ: This commit contains the following changes:\n", "",
-                indent("JJ:     ", diff.stat(72)),
-            ),
-            )
-          '';
-          log_node = ''
-            coalesce(
-              if(!self, "🮀"),
-              if(current_working_copy, "@"),
-              if(root, "┴"),
-              if(immutable, "●", "○"),
-            )
-          '';
-          op_log_node = ''if(current_operation, "@", "○")'';
         };
       };
     };
