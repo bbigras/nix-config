af7c0c21c08068d88224c5bb0960708435a41632
Bruno Bigras <bigras.bruno@gmail.com>
2024-08-20 21:42:16 -0400

    laptop: remove 'ac' and 'battery' targets

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -168,87 +168,10 @@ in
     };
   };
 
-  systemd.targets = {
-    ac = {
-      description = "On AC power";
-      unitConfig.DefaultDependencies = false;
-    };
-    battery = {
-      description = "On battery power";
-      unitConfig.DefaultDependencies = false;
-    };
-  };
-
-  services = {
-    udev.extraRules = ''
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${config.systemd.package}/bin/systemctl start battery.target"
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${config.systemd.package}/bin/systemctl stop ac.target"
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${config.systemd.package}/bin/systemctl start ac.target"
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${config.systemd.package}/bin/systemctl stop battery.target"
-
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${config.systemd.package}/bin/systemctl --user --machine=bbigras@.host start battery.target"
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${config.systemd.package}/bin/systemctl --user --machine=bbigras@.host stop ac.target"
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${config.systemd.package}/bin/systemctl --user --machine=bbigras@.host start ac.target"
-      SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${config.systemd.package}/bin/systemctl --user --machine=bbigras@.host stop battery.target"
-    '';
-  };
-
-  systemd.timers = {
-    logrotate.partOf = [ "ac.target" ];
-    logrotate.wantedBy = [ "ac.target" ];
-    systemd-tmpfiles-clean.partOf = [ "ac.target" ];
-    systemd-tmpfiles-clean.wantedBy = [ "ac.target" ];
-    fstrim.partOf = [ "ac.target" ];
-    fstrim.wantedBy = [ "ac.target" ];
-  };
-
   home-manager.users.bbigras = {
     imports = [
       ../../users/bbigras/trusted
     ];
-
-    systemd.user.targets = {
-      ac = {
-        Unit = {
-          Description = "On AC power";
-        };
-      };
-      battery = {
-        Unit = {
-          Description = "On battery power";
-        };
-      };
-    };
-
-    systemd.user.services = {
-      brightness-ac = {
-        Unit = {
-          Description = "100% brightness on ac";
-          PartOf = [ "ac.target" ];
-        };
-        Service = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.brillo}/bin/brillo -e -S 100";
-        };
-        Install = {
-          WantedBy = [ "ac.target" ];
-        };
-      };
-
-      brightness-battery = {
-        Unit = {
-          Description = "lower brightness on battery";
-          PartOf = [ "battery.target" ];
-        };
-        Service = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.brillo}/bin/brillo -e -S 50";
-        };
-        Install = {
-          WantedBy = [ "battery.target" ];
-        };
-      };
-    };
 
     services.gnome-keyring = {
       enable = true;
