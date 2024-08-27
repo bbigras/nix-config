87d5f32f754ad627676ac4b0fc512f170f4a1748
Bruno Bigras <bigras.bruno@gmail.com>
2024-08-27 01:12:43 -0400

    pixel6: use catppuccin-mocha

--- hosts/pixel6/default.nix
+++ hosts/pixel6/default.nix
@@ -49,6 +49,37 @@ in
     '';
   };
 
+  terminal = {
+    colors = {
+      background = "#1e1e2e";
+      foreground = "#cdd6f4";
+
+      color0 = "#45475a";
+      color8 = "#585b70";
+
+      color1 = "#f38ba8";
+      color9 = "#f38ba8";
+
+      color2 = "#a6e3a1";
+      color10 = "#a6e3a1";
+
+      color3 = "#f9e2af";
+      color11 = "#f9e2af";
+
+      color4 = "#89b4fa";
+      color12 = "#89b4fa";
+
+      color5 = "#f5c2e7";
+      color13 = "#f5c2e7";
+
+      color6 = "#94e2d5";
+      color14 = "#94e2d5";
+
+      color7 = "#bac2de";
+      color15 = "#a6adc8";
+    };
+  };
+
   # Read the changelog before changing this value
   system.stateVersion = "20.09";
 
