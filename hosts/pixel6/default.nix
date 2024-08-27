a2b3013d7f76402e04c20d90933028e106a4df0d
Bruno Bigras <bigras.bruno@gmail.com>
2024-08-27 01:26:25 -0400

    pixel6: remove nerdfonts

--- hosts/pixel6/default.nix
+++ hosts/pixel6/default.nix
@@ -91,21 +91,10 @@ in
   home-manager.useGlobalPkgs = true;
 
   home-manager.config =
-    { pkgs, config, lib, ... }:
+    { pkgs, lib, ... }:
     {
       # Read the changelog before changing this value
       home.stateVersion = "20.09";
-
-      home.activation = {
-        copyFont =
-          let
-            font_src = "${pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }}/share/fonts/truetype/NerdFonts/FiraCodeNerdFontMono-Regular.ttf";
-            font_dst = "${config.home.homeDirectory}/.termux/font.ttf";
-          in
-          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-            ( test ! -e "${font_dst}" || test $(sha1sum "${font_src}"|cut -d' ' -f1 ) != $(sha1sum "${font_dst}" |cut -d' ' -f1)) && $DRY_RUN_CMD install $VERBOSE_ARG -D "${font_src}" "${font_dst}"
-          '';
-      };
 
       imports = [
         ../../users/bbigras/core/atuin.nix
