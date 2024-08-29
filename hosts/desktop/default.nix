3802bbebcefa68b819797f5804516b115860029c
Bruno Bigras <bigras.bruno@gmail.com>
2024-08-29 12:45:05 -0400

    desktop: add ollama

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -180,6 +180,15 @@ in
   #   enableWebUI = true;
   # };
 
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1031"; # used to be necessary, but doesn't seem to anymore
+    };
+    rocmOverrideGfx = "10.3.1";
+  };
+
   environment.systemPackages = with pkgs; [
     fwupd
   ];
