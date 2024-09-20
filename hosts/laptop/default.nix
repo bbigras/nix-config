f5fe01e1f6630dd3a66ba9233d6e169db52ec889
Bruno Bigras <bigras.bruno@gmail.com>
2024-09-19 22:06:12 -0400

    laptop: remove tlp to fix build

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -141,25 +141,6 @@ in
   services = {
     fwupd.enable = true;
     thermald.enable = true;
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-
-        #Optional helps save long term battery health
-        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-      };
-    };
   };
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
