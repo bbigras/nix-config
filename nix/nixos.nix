9e354518aaf5e1f006ffe900a1d9098ccfc19482
Bruno Bigras <bigras.bruno@gmail.com>
2024-11-24 01:24:59 -0500

    remove lix for now

--- nix/nixos.nix
+++ nix/nixos.nix
@@ -7,7 +7,6 @@ let
     withSystem hostPlatform ({ pkgs, ... }:
       lib.nixosSystem {
         modules = [
-          inputs.lix-module.nixosModules.default
           inputs.nixos-cosmic.nixosModules.default
           (../hosts + "/${hostname}")
           {
