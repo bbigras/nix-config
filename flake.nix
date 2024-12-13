2ba0e5ead5e5f4a148aebc0730b0c9b04ca942f6
Bruno Bigras <bigras.bruno@gmail.com>
2024-12-13 10:55:11 -0500

    remove lix to fix build

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
       "https://nix-community.cachix.org"
       "https://nix-on-droid.cachix.org"
       "https://pre-commit-hooks.cachix.org"
-      "https://cache.lix.systems"
       "https://cosmic.cachix.org"
     ];
     extra-trusted-public-keys = [
@@ -15,7 +14,6 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
     ];
   };
@@ -50,16 +48,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs_zed.url = "github:nixos/nixpkgs?rev=d233eb89118bcc22856ac7d511ef56c146567c8b";
     nixpkgs_jj-fzf.url = "github:bbigras/nixpkgs/push-qsnkmvlwnrvm";
-
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-    lix-module = {
-      url = "git+https://git.lix.systems/lix-project/nixos-module";
-      inputs.lix.follows = "lix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
 
