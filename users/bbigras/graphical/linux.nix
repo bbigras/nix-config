31788f1c94e641afb5ae5f7cc68e4dbe60b732d9
Bruno Bigras <bigras.bruno@gmail.com>
2024-12-19 12:19:38 -0500

    set gtk4.extraConfig.gtk-application-prefer-dark-theme

--- users/bbigras/graphical/linux.nix
+++ users/bbigras/graphical/linux.nix
@@ -43,6 +43,7 @@
     enable = true;
     gtk2.extraConfig = "gtk-application-prefer-dark-theme = true";
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
 
   qt = {
