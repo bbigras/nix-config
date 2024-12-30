bde83dc28892233decfc0c2d9fdfc872ce59949f
Bruno Bigras <bigras.bruno@gmail.com>
2024-12-30 11:58:30 -0500

    remove alacritty

--- users/bbigras/core/tmux.nix
+++ users/bbigras/core/tmux.nix
@@ -28,8 +28,6 @@
     extraConfig = ''
       # update the env when attaching to an existing session
       set -g update-environment -r
-      set -ag terminal-overrides ",alacritty*:RGB,foot*:RGB,xterm-kitty*:RGB,xterm-256color:RGB"
-      set -as terminal-features ",alacritty*:RGB,foot*:RGB,xterm-kitty*:RGB,xterm-256color:RGB"
 
       # automatically renumber windows
       set -g renumber-windows on
--- users/bbigras/graphical/alacritty.nix
+++
@@ -1,3 +0,0 @@
-{
-  programs.alacritty.enable = true;
-}
