1bac08cbe67686bb39f7856716a075f5d6204e52
Bruno Bigras <bigras.bruno@gmail.com>
2025-01-01 21:14:48 -0500

    use home-manager ghostty module

--- users/bbigras/core/default.nix
+++ users/bbigras/core/default.nix
@@ -156,7 +156,6 @@
       aider-chat
       git-annex
       git-remote-gcrypt
-      ghostty
     ];
     shellAliases = {
       cls = "clear";
@@ -179,6 +178,40 @@
     carapace.enable = true;
     chromium.enable = true;
     eza.enable = true;
+    ghostty = {
+      enable = true;
+      settings = {
+        theme = "catppuccin-mocha";
+        font-size = 10;
+      };
+      themes = {
+        catppuccin-mocha = {
+          palette = [
+            "0=#45475a"
+            "1=#f38ba8"
+            "2=#a6e3a1"
+            "3=#f9e2af"
+            "4=#89b4fa"
+            "5=#f5c2e7"
+            "6=#94e2d5"
+            "7=#bac2de"
+            "8=#585b70"
+            "9=#f38ba8"
+            "10=#a6e3a1"
+            "11=#f9e2af"
+            "12=#89b4fa"
+            "13=#f5c2e7"
+            "14=#94e2d5"
+            "15=#a6adc8"
+          ];
+          background = "1e1e2e";
+          foreground = "cdd6f4";
+          cursor-color = "f5e0dc";
+          selection-background = "353749";
+          selection-foreground = "cdd6f4";
+        };
+      };
+    };
     jq.enable = true;
     kubecolor = {
       enable = true;
