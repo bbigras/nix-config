39e47ef66850069107385ed8f107f059ee1ff805
Bruno Bigras <bigras.bruno@gmail.com>
2025-02-01 20:45:27 -0500

    remove polkit-gnome

--- users/bbigras/graphical/linux.nix
+++ users/bbigras/graphical/linux.nix
@@ -71,23 +71,4 @@
     # };
     gpg-agent.pinentryPackage = pkgs.pinentry-gnome3;
   };
-
-  systemd.user.services = {
-    polkit-gnome = {
-      Unit = {
-        Description = "polkit-gnome";
-        Documentation = [ "man:polkit(8)" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        RestartSec = 3;
-        Restart = "always";
-      };
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-    };
-  };
 }
