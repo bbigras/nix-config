44bf11c26074da0fb17e6be1fd3f2aa8a89b908a
Bruno Bigras <bigras.bruno@gmail.com>
2025-02-14 14:03:21 -0500

    update zed-editor

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs_zed.url = "github:nixos/nixpkgs?rev=10952c87e367d3af7b877f79c41905429a4000b1";
+    nixpkgs_zed.url = "github:nixos/nixpkgs?rev=2c12bbf0e69660d6fdd936aa5ac260f65294d98b";
     nixpkgs_jj-fzf.url = "github:bbigras/nixpkgs?rev=801551c91747e725d9e9186919a4f475fa1bfca2";
 
     nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
