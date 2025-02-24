105ad916e83b75928bbc83ead836b5c25635b5f5
Bruno Bigras <bigras.bruno@gmail.com>
2025-02-24 14:26:48 -0500

    laptop: add wluma

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -153,5 +153,39 @@ in
       enable = true;
       components = [ "secrets" ];
     };
+    services.wluma = {
+      enable = true;
+      settings = {
+        als = {
+          webcam = {
+            video = 0;
+            thresholds = {
+              "0" = "night";
+              "15" = "dark";
+              "30" = "dim";
+              "45" = "normal";
+              "60" = "bright";
+              "75" = "outdoors";
+            };
+          };
+        };
+
+        output.backlight = [
+          {
+            name = "eDP-1";
+            path = "/sys/class/backlight/intel_backlight";
+            # capturer = "wayland";
+            capturer = "none";
+          }
+        ];
+
+        keyboard = [
+          {
+            name = "keyboard-dell";
+            path = "/sys/bus/platform/devices/dell-laptop/leds/dell::kbd_backlight";
+          }
+        ];
+      };
+    };
   };
 }
