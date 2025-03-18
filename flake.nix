ccbf2e11b873679d244322f3411996de65d8608d
Bruno Bigras <bigras.bruno@gmail.com>
2025-03-18 00:01:25 -0400

    Back out "pin nixpkgs"
    This backs out commit e69b4070395e556bc16d1e29ef266612e43050d1.

--- flake.nix
+++ flake.nix
@@ -111,7 +111,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=e3e32b642a31e6714ec1b712de8c91a3352ce7e1";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
