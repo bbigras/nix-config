35d3c386151ae15f36248c651b8fdce3e0383510
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2025-05-22 01:01:07 -0400

    add vesktop

--- users/bbigras/core/default.nix
+++ users/bbigras/core/default.nix
@@ -229,6 +229,38 @@
         twitch-low-latency = true;
       };
     };
+    vesktop = {
+      enable = true;
+      settings = {
+        appBadge = false;
+        arRPC = true;
+        checkUpdates = false;
+        customTitleBar = false;
+        disableMinSize = true;
+        minimizeToTray = false;
+        tray = false;
+        splashBackground = "#000000";
+        splashColor = "#ffffff";
+        splashTheming = true;
+        staticTitle = true;
+        hardwareAcceleration = true;
+        discordBranch = "stable";
+      };
+      vencord.settings = {
+        autoUpdate = false;
+        autoUpdateNotification = false;
+        notifyAboutUpdates = false;
+        useQuickCss = true;
+        disableMinSize = true;
+        plugins = {
+          # MessageLogger = {
+          #   enabled = true;
+          #   ignoreSelf = true;
+          # };
+          FakeNitro.enabled = true;
+        };
+      };
+    };
     tealdeer.enable = true;
     zoxide.enable = true;
   };
