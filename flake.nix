68151066f67ebdd2e359a0d22818b52a7e05d9fb
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2025-06-03 13:37:15 -0400

    update veilid

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs_veilid.url = "github:nixos/nixpkgs?rev=3bfe0a499ee44209ea2df5fce54d2d0ff2552305";
+    nixpkgs_veilid.url = "github:nixos/nixpkgs?rev=3b4f21beac1472f26dfaee39e58020b8dfa11e78";
 
     nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
 
