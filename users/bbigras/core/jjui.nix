52d0d6773936a75323288f2b58a08d1ea99c96d5
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2025-09-12 21:30:26 -0400

    use the jjui home-manager module

--- users/bbigras/core/jjui.nix
+++ users/bbigras/core/jjui.nix
@@ -1,58 +1,62 @@
-{ pkgs, ... }:
+{ ... }:
 
-let
-  yaml = pkgs.formats.toml { };
-in
 {
-  xdg.configFile."jjui/config.toml".source = yaml.generate "config.toml" {
-    custom_commands = {
-      "show diff" = {
-        key = [ "U" ];
-        args = [
-          "diff"
-          "-r"
-          "$change_id"
-          "--color"
-          "always"
-        ];
-        show = "diff";
+  programs.jjui = {
+    enable = true;
+    settings = {
+      revisions = {
+        template = "builtin_log_compact";
+        # revset = "ancestors(@ | heads(remote_bookmarks())) ~ empty()";
       };
-      "show oplog diff" = {
-        key = [ "ctrl+o" ];
-        args = [
-          "op"
-          "show"
-          "$operation_id"
-          "--color"
-          "always"
-        ];
-        show = "diff";
-      };
-      "resolve vscode" = {
-        key = [ "R" ];
-        args = [
-          "resolve"
-          "--tool"
-          "vscode"
-        ];
-        show = "interactive";
-      };
-      "new main" = {
-        args = [
-          "new"
-          "main"
-        ];
-      };
-      "tug" = {
-        key = [ "ctrl+t" ];
-        args = [
-          "bookmark"
-          "move"
-          "--from"
-          "closest_bookmark($change_id)"
-          "--to"
-          "closest_pushable($change_id)"
-        ];
+      custom_commands = {
+        "show diff" = {
+          key = [ "U" ];
+          args = [
+            "diff"
+            "-r"
+            "$change_id"
+            "--color"
+            "always"
+          ];
+          show = "diff";
+        };
+        "show oplog diff" = {
+          key = [ "ctrl+o" ];
+          args = [
+            "op"
+            "show"
+            "$operation_id"
+            "--color"
+            "always"
+          ];
+          show = "diff";
+        };
+        "resolve vscode" = {
+          key = [ "R" ];
+          args = [
+            "resolve"
+            "--tool"
+            "vscode"
+          ];
+          show = "interactive";
+        };
+        "new main" = {
+          args = [
+            "new"
+            "main"
+          ];
+        };
+        "tug" = {
+          key = [ "ctrl+t" ];
+          args = [
+            "bookmark"
+            "move"
+            "--from"
+            "closest_bookmark($change_id)"
+            "--to"
+            "closest_pushable($change_id)"
+          ];
+        };
       };
     };
   };
