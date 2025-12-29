71bdb70e986a14192cc57516fd2c96912587a63b
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2025-12-29 01:47:51 -0500

    disable git signing

--- modules/home/trusted/default.nix
+++ modules/home/trusted/default.nix
@@ -2,10 +2,10 @@
 {
   imports = with flake.self.homeModules; [ trusted-gpg ];
 
-  programs.git.settings = {
-    commit.gpgsign = true;
-    gpg.format = "ssh";
-    gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-    user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQgTWfmR/Z4Szahx/uahdPqvEP/e/KQ1dKUYLenLuY2";
-  };
+  # programs.git.settings = {
+  #   commit.gpgsign = true;
+  #   gpg.format = "ssh";
+  #   gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+  #   user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKJvfEJv2UsKPnfP9fHIwTuGsMWXc4wHYMf7HZlq2vY";
+  # };
 }
