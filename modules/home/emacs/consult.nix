54d10f6ed3596da887b945c58ea09a14e2f444ef
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2026-01-16 00:33:53 -0500

    emacs: fix warnings

--- modules/home/emacs/consult.nix
+++ modules/home/emacs/consult.nix
@@ -99,8 +99,6 @@
                          consult-theme :preview-key '(:debounce 0.2 any)
                          consult-ripgrep consult-git-grep consult-grep
                          consult-bookmark consult-recent-file consult-xref
-                         consult--source-bookmark consult--source-file-register
-                         consult--source-recent-file consult--source-project-recent-file
                          ;; :preview-key "M-."
                          :preview-key '(:debounce 0.4 any))
 
