d70be8ef3fdb0b36bbdcaaec3fc5730f11692a77
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2026-05-12 12:29:23 -0400

    laptop: add nixosModules.graphical-fonts

--- configurations/nixos/laptop/default.nix
+++ configurations/nixos/laptop/default.nix
@@ -16,6 +16,7 @@ in
     self.nixosModules.graphical
     self.nixosModules.graphical-cosmic
     self.nixosModules.graphical-steam
+    self.nixosModules.graphical-fonts
     self.nixosModules.graphical-trusted
     self.nixosModules.pam-limits
     self.nixosModules.hardware-secureboot
