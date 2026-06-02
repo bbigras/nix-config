29a821a843d0128ebcb0e374f7ca153819bdde20
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2026-06-02 16:33:36 -0400

    fish: add tmux_refresh_env function

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -11,6 +11,21 @@
         ${pkgs.nix-your-shell}/bin/nix-your-shell --nom fish | source
 
         fish_default_key_bindings
+
+        function tmux_refresh_env --on-event fish_prompt
+            test -n "$TMUX"; or return
+
+            for var in SSH_AUTH_SOCK SSH_AGENT_PID SSH_CONNECTION SSH_CLIENT SSH_TTY
+                set line (tmux show-environment "$var" 2>/dev/null)
+                or continue
+
+                if string match -q -- "-$var" "$line"
+                    set -e $var
+                else
+                    set -gx $var (string replace "$var=" "" "$line")
+                end
+            end
+        end
       '')
     ];
     plugins = [
