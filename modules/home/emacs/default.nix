5a5620b2fca5e8ace89a346bd6c3b38d8b84f0ef
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2026-06-27 16:49:17 -0400

    markdown-mode -> markdown-ts-mode

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -1504,18 +1504,9 @@ in
           };
         };
 
-        markdown-mode = {
+        markdown-ts-mode = {
           enable = true;
-          # extraPackages = [ pkgs.marksman ];
-          config = ''
-            (setq markdown-command "marked")
-            (dolist (face '((markdown-header-face-1 . 1.2)
-                             (markdown-header-face-2 . 1.1)
-                             (markdown-header-face-3 . 1.0)
-                             (markdown-header-face-4 . 1.0)
-                             (markdown-header-face-5 . 1.0)))
-               (set-face-attribute (car face) nil :weight 'normal :height (cdr face)))
-          '';
+          defer = true;
         };
 
         # pandoc-mode = {
