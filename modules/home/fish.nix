3be90b4968a0e20cca558ba9a1afb73d61d9a940
Bruno Bigras <24027+bbigras@users.noreply.github.com>
2026-08-11 13:59:16 -0400

    fish: add fishPlugins.done

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -11,12 +11,19 @@
         ${pkgs.nix-your-shell}/bin/nix-your-shell --nom fish | source
 
         fish_default_key_bindings
+
+        set -U __done_min_cmd_duration 5000
+        set -U __done_notification_command "~/dev/send_pushover.sh \$title \$message"
       '')
     ];
     plugins = [
       {
         name = "autopair";
         inherit (pkgs.fishPlugins.autopair) src;
+      }
+      {
+        name = "done";
+        inherit (pkgs.fishPlugins.done) src;
       }
     ];
   };
